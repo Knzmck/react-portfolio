b0ab012e6318eeb487b690634fd96cd14d5d8cee
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-30 17:09:35 -0500

    add footer

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -19,18 +19,17 @@ class About extends Component {
                                 <img src={ProfilePic} alt="picture of Mackenzie." className="profilepic mr-5 mt-5"></img>
                                 <section className="media-body">
                                     <p className="description">
-                                        <h3><b>Hello!</b></h3>
-                                        <h5>Allow me to introduce myself...</h5>
+                                        <br></br>
+                                        <h3><b>Hello! Allow me to introduce myself...</b></h3>
                         My name is Mackenzie Schutz. I am a Registered Nurse, Marketing Consultant & Full Stack Web Developer [in training] from the
                         Kansas City area.
                                     </p>
-                                    <button className="btn btn-info" id="resume-btn" onClick={this.handleClick}>See My Full Resume Here</button>
                                 </section>
-
                             </div>
                         </div>
                     </div>
                 </div>
+                <button className="btn btn-info" id="resume-btn" onClick={this.handleClick}>See My Full Resume Here</button>
             </div>
         );
     }
