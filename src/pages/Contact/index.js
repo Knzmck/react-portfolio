21d3c856ebf910da5458717af5eda9a03084924e
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-30 17:39:50 -0500

    adjust styling on contact pg

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -8,19 +8,14 @@ class Contact extends Component {
         return (
             <main className="card">
                 <div className="card-body">
-                    <div className="mb-4" id="container2">
-                        <section className="mb-4">
+                    <div className="mb-4" id="container2">                       
                             <h2 className="contact-header">Contact me</h2>
                             <div className="row">
                                 <div className="col-md-9 mb-md-0 mb-5">
                                     <ContactForm />
-                                    <div class="text-center text-md-left">
-                                        <a class="btn btn-info">Send</a>
-                                    </div>
                                 </div>
                                 <ContactInfo />
                             </div>
-                        </section>
                     </div>
                 </div>
             </main>
