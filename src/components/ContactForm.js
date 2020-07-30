e3472b03904cdd72ae5b351e4d280941a0f2aa73
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-30 17:28:12 -0500

    add contact page components

---
+++ src/components/ContactForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class ContactForm extends Component {
+    render() {
+        return (
+            <form >
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="name" name="name" class="form-control"></input>
+                        <label for="name">Your
+                        name</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="email" name="email" class="form-control"></input>
+                        <label for="email">Your
+                        email</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="md-form mb-0">
+                        <input type="text" id="subject" name="subject" class="form-control"></input>
+                        <label for="subject"
+                        >Subject</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="md-form">
+                        <textarea type="text" id="message" name="message" rows="2"
+                            class="form-control md-textarea"></textarea>
+                        <label for="message" >Your
+                        message</label>
+                    </div>
+                </div>
+            </div>
+        </form>
+        );
+    }
+}
+
+export default ContactForm;
