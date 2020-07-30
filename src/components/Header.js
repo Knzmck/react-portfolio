6838bb4641beb372abc565d5184164d4ec483cc5
Mackenzie Schutz <mshoots64@gmail.com>
2020-07-30 16:12:18 -0500

    add header to other pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <h1 class="main-header">Mackenzie Schutz</h1>
         <nav class="navbar navbar-light bg-light">
             <form class="form-inline">
-                <button className="btn btn-outline-info" type="button"> <Link to= "/" >About </Link></button>
+                <button className="btn btn-outline-info" type="button" component= { Link } to="/about"> <Link to= "/" >About </Link></button>
                 <button class="btn btn-sm btn-outline-secondary" type="button"><Link to= "/portfolio" >Portfolio </Link></button>
                 <button class="btn btn-sm btn-outline-secondary" type="button"><Link to= "/contact" >Contact</Link></button>
             </form>
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from "../../components/Header";
 
 class Contact extends Component {
     render() {
         return (
             <main>
-                
+                <Header />
             </main>
         );
     }
