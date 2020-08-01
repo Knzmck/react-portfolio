bf4ae6b3b83227df7dab0f704830035a1a69f246
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-01 15:49:41 -0500

    add map to portfolio btns

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import ProfilePic from '../assets/weather.jpg'
+import { Link } from 'react-router-dom'
 
-class Project extends Component {
-    render() {
-        return (
-            <div className="card">
-                <img src={ProfilePic} alt="picture of Mackenzie." className="profilepic mr-5 mt-5"></img>
-                <button className="btn btn1" id="weather-btn">Weather Dashboard App</button>
-                <button className="btn btn2" id="weather-repo"> Repository </button>
-            </div>
-        );
-    }
+function Project(props) {
+    return (
+        // <div className="card">
+        //     <img src={ProfilePic} alt="picture of Mackenzie." className="profilepic mr-5 mt-5"></img>
+        //     <button className="btn btn1" id="weather-btn">Weather Dashboard App</button>
+        //     <button className="btn btn2" id="weather-repo"> Repository </button>
+        // </div>
+        <ul>
+            {props.projects.map(item => (
+                <li className="row" key={item.id}>
+                    {item.title}
+                    <img className="project-icon" src= {item.picture}></img>
+                    <Link className="col" to={item.repo}> Repo</Link>
+                    <Link className="col" to={item.repo}> App</Link>
+                </li>
+                // <li key={item.id}> 
+                // {item.title}
+                //  </li>
+            ))}
+        </ul>
+    );
 }
 
 export default Project;
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import Project from "../../components/Project";
+import WeatherPic from "../../assets/weather.jpg"
 
+const portfolioInfo = [
+    {
+        id: 1,
+        title: "Weather App",
+        picture: WeatherPic,
+        repo: "https://github.com/Knzmck/weather-dashboard.git",
+        app: "https://knzmck.github.io/weather-dashboard/"
+    },
+    {
+        id: 2,
+        title: "Weather App",
+        picture: WeatherPic,
+        repo: "https://github.com/Knzmck/weather-dashboard.git",
+        app: "https://knzmck.github.io/weather-dashboard/"
+    }
+
+]
 
 class Portfolio extends Component {
+    
     render() {
         return (
             <div>
                 <main className="card container1" id="container1">
                     <div className="row">
-                        <Project />
+                        <Project projects={portfolioInfo} />
                     </div>
                 </main>
             </div>
