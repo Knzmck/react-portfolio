90884b99122bc09caaf9b9ce90f9adc777ee9680
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-07 16:01:22 -0500

    more css and styling changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ function Header() {
         <h1 class="main-header">Mackenzie Schutz</h1>
         <nav class="navbar navbar-info">
             <form class="form-inline">
-                <button className="btn btn-outline-danger" type="button" component= { Link } to="/about"> <Link to= "/" >About </Link></button>
-                <button class="btn btn-sm btn-danger" type="button"><Link to= "/portfolio" >Portfolio </Link></button>
-                <button class="btn btn-sm btn-danger" type="button"><Link to= "/contact" >Contact</Link></button>
+                <button className="navbtn btn btn-outline-dark" type="button" component= { Link } to="/about"> <Link to= "/" >About </Link></button>
+                <button class="navbtn btn btn-sm btn-outline-dark" type="button"><Link to= "/portfolio" >Portfolio </Link></button>
+                <button class="navbtn btn btn-sm btn-outline-dark" type="button"><Link to= "/contact" >Contact</Link></button>
             </form>
         </nav>
     </header>
