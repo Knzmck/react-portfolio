9df0a7f14d4dd59fc9328fc49382b24287750e99
Mackenzie Schutz <mshoots64@gmail.com>
2020-09-09 12:56:49 -0500

    attempt to fix contact card issue

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Icon from './components/Icon';
 import ContactInfo from './components/ContactInfo';
+import ProfilePic from '../About/croppedprofpic.jpg'
 import "./style.css";
 
 const iconInfo = [
@@ -28,7 +29,17 @@ class Contact extends Component {
                 <div className="contactpg card-body">
                     <h2 className="contact-header">Let's connect!</h2>
                     <div className="row no-gutters">
-                        <ContactInfo />
+                        <div className="col-12 col-sm-6 col-md-8 mb-4 card contact-card">
+                            <div className="row">
+                                <img src={ProfilePic} alt="picture of Mackenzie." className="contactpic mr-5 mt-5"></img>
+                                <text className="mt-5 ml-3 contact-info">
+                                    <h3>Mackenzie Schutz</h3>
+                                    <h5 className="mt-2">Full Stack Web Developer</h5>
+                                    <h6 className="mt-3">mshoots64@gmail.com</h6>
+                                    <h6 className="mt-2">C. 913-660-2003</h6>
+                                </text>
+                            </div>
+                        </div>
                         <div className="col-6 col-md-4">
                             <Icon icons={iconInfo} />
                         </div>
